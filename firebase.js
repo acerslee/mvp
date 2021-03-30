285e068981ceee729663e48f0dbd5514d4edaae0
Alex Lee <ahjlee712@gmail.com>
2021-03-29 21:27:19 -0400

    Created env

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+const app = firebase.initializeApp({
+  apiKey:  process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+});
+
+const fireAuthorization = app.auth();
+
+export { fireAuthorization, app };
