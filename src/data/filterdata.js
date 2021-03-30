61b5090812311466924b0cc06d95a636ec75ba6f
Alex Lee <ahjlee712@gmail.com>
2021-03-30 17:29:26 -0400

    Filters work. Need to fix slider

---
+++ src/data/filterdata.js
@@ -0,0 +1,24 @@
+const filterOptions = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 50,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 50,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  }
+];
+
+module.exports = filterOptions;
--- src/pages/components/editor.js
+++ src/pages/components/editor.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import Filters from './filter.js';
-// import Slider from './slider.js';
+import React, { useState } from 'react';
+// import Filters from './filter.js';
+import filterOptions from '../../data/filterdata.js';
 import { StaticImage } from 'gatsby-plugin-image';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import Button from '@material-ui/core/Button';
 import '../../styles/editor.css';
+import FilterItem from './filterItem.js';
+import Slider from './slider.js';
 
 const Editor = () => {
+
+  const [filters, setFilters] = useState(filterOptions)
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const selectedFilter = filters[selectedIndex];
+
+  const filterSlider = event => {
+    setFilters(prevOptions => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedIndex) return option
+        return { ...option, value: event.target.value}
+      })
+    });
+  };
+
+  const setImageFilters = () => {
+    const styles = filters.map(filter => {
+      return `${filter.property}(${filter.value}${filter.unit})`
+    })
+
+    return { filter: styles.join(' ') };
+  }
 
   return(
     <div id = 'photo-editor'>
@@ -27,13 +51,28 @@ const Editor = () => {
               className = 'edit-image'
               src = '../../images/pineapples.jpg'
               alt = 'pineapples'
-              // style = {setImageFilters()}
+              style = {setImageFilters()}
             />
           </TransformComponent>
         </React.Fragment>
       )}
       </TransformWrapper>
-      <Filters />
+      <div className = 'filters'>
+        {filters.map((filter, index) => (
+          <FilterItem
+            key = {index}
+            name = {filter.name}
+            active = {index === selectedIndex}
+            handleClick = {() => setSelectedIndex(index)}
+          />
+        ))}
+        <Slider
+          min = {selectedFilter.range.min}
+          max = {selectedFilter.range.max}
+          value = {selectedFilter.range.value}
+          filterSlider = {filterSlider}
+        />
+      </div>
     </div>
   )
 };
