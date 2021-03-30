e8a419e8f3b3b689c5b7111892f2f1f1a5c8e17e
Alex Lee <ahjlee712@gmail.com>
2021-03-30 17:37:49 -0400

    Fixed slider. Image editing properly

--- src/pages/components/editor.js
+++ src/pages/components/editor.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-// import Filters from './filter.js';
 import filterOptions from '../../data/filterdata.js';
+import FilterItem from './filterItem.js';
+import Slider from './slider.js';
 import { StaticImage } from 'gatsby-plugin-image';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import Button from '@material-ui/core/Button';
 import '../../styles/editor.css';
-import FilterItem from './filterItem.js';
-import Slider from './slider.js';
 
 const Editor = () => {
 
@@ -17,9 +16,9 @@ const Editor = () => {
 
   const filterSlider = event => {
     setFilters(prevOptions => {
-      return prevOptions.map((option, index) => {
-        if (index !== selectedIndex) return option
-        return { ...option, value: event.target.value}
+      return prevOptions.map((filter, index) => {
+        if (index !== selectedIndex) return filter
+        return { ...filter, value: event.target.value}
       })
     });
   };
@@ -69,7 +68,7 @@ const Editor = () => {
         <Slider
           min = {selectedFilter.range.min}
           max = {selectedFilter.range.max}
-          value = {selectedFilter.range.value}
+          value = {selectedFilter.value}
           filterSlider = {filterSlider}
         />
       </div>
